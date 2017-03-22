4de5fd6c6723934c2a096205e0ee9d57a36300bf
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-03-22 13:50:25 +0100

    bis

--- 15_CDIO_D3/src/businessLayer/IBusinessLayer.java
+++ 15_CDIO_D3/src/businessLayer/IBusinessLayer.java
@@ -1,5 +1,16 @@
 package businessLayer;
 
+import java.util.List;
+
+import datalayer.OperatoerDTO;
+import exceptions.DALException;
+
 public interface IBusinessLayer {
 
+	OperatoerDTO getUser(int userId) throws DALException;
+	List<OperatoerDTO> getUserList() throws DALException;
+	void createUser(OperatoerDTO user) throws DALException;
+	void updateUser(OperatoerDTO user) throws DALException;
+	void deleteUser(int userId) throws DALException;
+	
 }
