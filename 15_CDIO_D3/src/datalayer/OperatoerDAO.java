f876c113ff96d50c4eefe5ab7310c7f8ccce94d0
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-03-22 16:33:06 +0100

    Fejl i updateOperatoer

--- 15_CDIO_D3/src/datalayer/OperatoerDAO.java
+++ 15_CDIO_D3/src/datalayer/OperatoerDAO.java
@@ -7,62 +7,99 @@ import java.util.ArrayList;
 
 import exceptions.DALException;
 
-
 public class OperatoerDAO implements IOperatoerDAO {
-	
-	
-	
+
 	public OperatoerDTO getOperatoer(int oprId) throws DALException {
 		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer natural join roles WHERE opr_id = " + oprId);
-	    try {
-	    	if (!rs.first()) throw new DALException("Operatoeren " + oprId + " findes ikke");
-	    	OperatoerDTO t=new OperatoerDTO (rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), rs.getString("cpr"), rs.getString("password"), 
-	    			new ArrayList<String>());
-	    	t.addRole(rs.getString("role"));
-	    	while(rs.next()){
-	    		t.addRole(rs.getString("role"));
-	    	}
-	    	return t;
-	    }
-	    catch (SQLException e) {throw new DALException(e); }
-		
+		try {
+			if (!rs.first())
+				throw new DALException("Operatoeren " + oprId + " findes ikke");
+			OperatoerDTO t = new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"),
+					rs.getString("cpr"), rs.getString("password"), new ArrayList<String>());
+			t.addRole(rs.getString("role"));
+			while (rs.next()) {
+				t.addRole(rs.getString("role"));
+			}
+			return t;
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
+
 	}
-	
-	public void createOperatoer(OperatoerDTO opr) throws DALException {		
+
+	public void createOperatoer(OperatoerDTO opr) throws DALException {
+		Connector.doUpdate("INSERT INTO operatoer(opr_id, opr_navn, ini, cpr, password) VALUES " + "(" + opr.getOprId()
+				+ ", '" + opr.getOprNavn() + "', '" + opr.getIni() + "', '" + opr.getCpr() + "', '" + opr.getPassword()
+				+ "')");
+		for (int i = 0; i < opr.getRoles().size(); i++) {
 			Connector.doUpdate(
-				"INSERT INTO operatoer(opr_id, opr_navn, ini, cpr, password) VALUES " +
-				"(" + opr.getOprId() + ", '" + opr.getOprNavn() + "', '" + opr.getIni() + "', '" + 
-				opr.getCpr() + "', '" + opr.getPassword() + "')"
-			);
+					"INSERT INTO roles(opr_id,role) VALUES (" + opr.getOprId() + ",'" + opr.getRoles().get(i) + "')");
+		}
 	}
-	
+
 	public void updateOperatoer(OperatoerDTO opr) throws DALException {
+		List<String> rigtigeRoller = opr.getRoles();
+		List<String> insertKo = opr.getRoles();
+		List<String> deleteKo = new ArrayList<String>(0);
 		Connector.doUpdate(
-				"UPDATE operatoer SET  opr_navn = '" + opr.getOprNavn() + "', ini =  '" + opr.getIni() + 
-				"', cpr = '" + opr.getCpr() + "', password = '" + opr.getPassword() + "' WHERE opr_id = " +
-				opr.getOprId()
-		);
+				"UPDATE operatoer SET  opr_navn = '" + opr.getOprNavn() + "', ini =  '" + opr.getIni() + "', cpr = '"
+						+ opr.getCpr() + "', password = '" + opr.getPassword() + "' WHERE opr_id = " + opr.getOprId());
+		ResultSet rs = Connector.doQuery("SELECT * FROM roles WHERE opr_id=" + opr.getOprId());
+		try {
+			rs.first();
+			for (int i = 0; i < rigtigeRoller.size(); i++) {
+				if (!(rigtigeRoller.get(i).contains(rs.getString("role")))) {
+					deleteKo.add(rs.getString("role"));
+				} else {
+					insertKo.remove(rs.getString("role"));
+				}
+			}
+			while (rs.next()) {
+				for (int i = 0; i < rigtigeRoller.size(); i++) {
+					if (!(rigtigeRoller.get(i).contains(rs.getString("role")))) {
+						deleteKo.add(rs.getString("role"));
+					} else {
+						insertKo.remove(rs.getString("role"));
+					}
+				}
+			}
+			if(!deleteKo.isEmpty()){
+				for(int i=0;i<deleteKo.size();i++){
+					Connector.doUpdate("DELETE FROM ROLE WHERE opr_id="+opr.getOprId()+" and role='"+deleteKo.get(i)+"'");
+				}
+			}
+			if(!insertKo.isEmpty()){
+				for(int i=0;i<insertKo.size();i++){
+					Connector.doUpdate(
+							"INSERT INTO roles(opr_id,role) VALUES (" + opr.getOprId() + ",'" + opr.getRoles().get(i) + "')");
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DALException(e);
+		}
 	}
-	
+
 	public List<OperatoerDTO> getOperatoerList() throws DALException {
 		List<OperatoerDTO> list = new ArrayList<OperatoerDTO>(1);
 		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer natural join roles order by opr_id");
-		int tempID=0;
-		try
-		{
-			while (rs.next()){
-				if(rs.getInt("opr_id")==tempID){
-					list.get(list.size()-1).addRole(rs.getString("role"));
-				}else{
-					OperatoerDTO t= new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), rs.getString("cpr"), rs.getString("password"), new ArrayList<String>());
+		int tempID = 0;
+		try {
+			while (rs.next()) {
+				if (rs.getInt("opr_id") == tempID) {
+					list.get(list.size() - 1).addRole(rs.getString("role"));
+				} else {
+					OperatoerDTO t = new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"),
+							rs.getString("ini"), rs.getString("cpr"), rs.getString("password"),
+							new ArrayList<String>());
 					t.addRole(rs.getString("role"));
 					list.add(t);
-					tempID=rs.getInt("opr_id");
+					tempID = rs.getInt("opr_id");
 				}
 			}
+		} catch (SQLException e) {
+			throw new DALException(e);
 		}
-		catch (SQLException e) { throw new DALException(e); }
 		return list;
 	}
 }
-	
--- 15_CDIO_D3/src/datalayer/TempMain.java
+++ 15_CDIO_D3/src/datalayer/TempMain.java
@@ -1,47 +1,64 @@
 package datalayer;
 
-
 import exceptions.DALException;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class TempMain {
 	public static void main(String[] args) {
-		try { new Connector(); } 
-		catch (InstantiationException e) { e.printStackTrace(); }
-		catch (IllegalAccessException e) { e.printStackTrace(); }
-		catch (ClassNotFoundException e) { e.printStackTrace(); }
-		catch (SQLException e) { e.printStackTrace(); }
+		try {
+			new Connector();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		OperatoerDAO opr = new OperatoerDAO();
-		System.out.println("Operatoer nummer 13:");
-		try { System.out.println(opr.getOperatoer(13)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-//		
-//		System.out.println("Indsaettelse af ny operatoer med opr_id =  34");
-//		OperatoerDTO oprDTO = new OperatoerDTO(34,"Don Juan","DJ","000000-0000","iloveyou");
-//		try { opr.createOperatoer(oprDTO); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }	
-//		
-//		System.out.println("Operatoer nummer 34:");
-//		try { System.out.println(opr.getOperatoer(34)); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }
-//		
-//		System.out.println("Opdatering af initialer for operatoer nummer 34");
-//		oprDTO.setIni("DoJu");
-//		try { opr.updateOperatoer(oprDTO); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }
-//		
-//		System.out.println("Operatoer nummer 34:");
-//		try { System.out.println(opr.getOperatoer(34)); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }
-		
-//		System.out.println("Alle operatoerer:");
-//		try { System.out.println(opr.getOperatoerList()); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }
-//		
-//		System.out.println("Operatoer nummer 5:");
-//		try { System.out.println(opr.getOperatoer(5)); }
-//		catch (DALException e) { System.out.println(e.getMessage()); }		
-//		
+		// System.out.println("Operatoer nummer 13:");
+		// try { System.out.println(opr.getOperatoer(13)); }
+		// catch (DALException e) { System.out.println(e.getMessage()); }
+		//
+		// System.out.println("Indsaettelse af ny operatoer med opr_id = 34");
+		// OperatoerDTO oprDTO = new OperatoerDTO(34,"Don
+		// Juan","DJ","000000-0000","iloveyou");
+		// try { opr.createOperatoer(oprDTO); }
+		// catch (DALException e) { System.out.println(e.getMessage()); }
+		//
+		System.out.println("Operatoer nummer 11:");
+		OperatoerDTO t;
+		try {
+			t = opr.getOperatoer(11);
+			System.out.println(opr.getOperatoer(11));
+
+			System.out.println("Opdatering af initialer for operatoer nummer 34");
+			List<String> tempList=t.getRoles();
+			tempList.remove(1);
+			t.setRoles(tempList);
+			System.out.println(t.getRoles());
+			opr.updateOperatoer(t);
+			t=opr.getOperatoer(11);
+			System.out.println(t);
+		} catch (DALException e) {
+			System.out.println(e.getMessage());
+		}
+
+		//
+		// System.out.println("Operatoer nummer 34:");
+		// try { System.out.println(opr.getOperatoer(34)); }
+		// catch (DALException e) { System.out.println(e.getMessage()); }
+
+		// System.out.println("Alle operatoerer:");
+		// try { System.out.println(opr.getOperatoerList()); }
+		// catch (DALException e) { System.out.println(e.getMessage()); }
+		//
+		// System.out.println("Operatoer nummer 5:");
+		// try { System.out.println(opr.getOperatoer(5)); }
+		// catch (DALException e) { System.out.println(e.getMessage()); }
+		//
 	}
 }
