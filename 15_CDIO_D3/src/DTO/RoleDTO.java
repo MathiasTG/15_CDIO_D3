67f5d7edfe24d11a8ddd35f99928e5aaa7a7bccc
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-03-29 11:49:25 +0200

    Created RoleDTO

---
+++ 15_CDIO_D3/src/DTO/RoleDTO.java
@@ -0,0 +1,31 @@
+package DTO;
+
+public class RoleDTO {
+
+	private int roleId;
+	private String roleName;
+
+	RoleDTO(int roleId, String roleName) {
+		this.roleId = roleId;
+		this.roleName = roleName;
+	}
+	
+	public void setRoleId(int id){
+		roleId = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+	
+	
+
+}
