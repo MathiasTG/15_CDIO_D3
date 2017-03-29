ea902ef3ce680364942be6cdc77d31ff528e9071
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-03-29 12:05:42 +0200

    s

---
+++ 15_CDIO_D3/src/datalayer/RoleDAO.java
@@ -0,0 +1,40 @@
+package datalayer;
+
+import java.util.List;
+
+import DTO.RoleDTO;
+import exceptions.DALException;
+
+public class RoleDAO implements IRoleDAO {
+
+	@Override
+	public RoleDTO getRole(int roleId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RoleDTO> getRoleList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createRole(RoleDTO role) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRole(RoleDTO role) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRole(RoleDTO role) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
