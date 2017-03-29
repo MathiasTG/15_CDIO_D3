5650b733c3fe34d6d3e24f6c7e96ddb5d322c042
s147300 <s147300@student.dtu.dk>
2017-03-29 12:04:38 +0200

    changes to operatoerDTO (from list of strings to roledto)

--- 15_CDIO_D3/src/DTO/OperatoerDTO.java
+++ 15_CDIO_D3/src/DTO/OperatoerDTO.java
@@ -9,52 +9,94 @@ import java.util.List;
  * @version 1.2
  */
 
-public class OperatoerDTO
-{
-	/** Operatoer-identifikationsnummer (opr_id) i omraadet 1-99999999. Vaelges af brugerne */
-	private int oprId;                     
+public class OperatoerDTO {
+	/**
+	 * Operatoer-identifikationsnummer (opr_id) i omraadet 1-99999999. Vaelges
+	 * af brugerne
+	 */
+	private int oprId;
 	/** Operatoernavn (opr_navn) min. 2 max. 20 karakterer */
-	private String oprNavn;                
+	private String oprNavn;
 	/** Operatoer-initialer min. 2 max. 3 karakterer */
-	private String ini;                 
+	private String ini;
 	/** Operatoer cpr-nr 10 karakterer */
-	private String cpr;                 
+	private String cpr;
 	/** Operatoer password min. 7 max. 8 karakterer */
-	private String password;  
-	private List<String> roles;
+	private String password;
+	private List<RoleDTO> roles;
 
-	public OperatoerDTO(int oprId, String oprNavn, String ini, String cpr, String password, List<String> roles)
-	{
+	public OperatoerDTO(int oprId, String oprNavn, String ini, String cpr, String password, List<RoleDTO> roles) {
 		this.oprId = oprId;
 		this.oprNavn = oprNavn;
 		this.ini = ini;
 		this.cpr = cpr;
 		this.password = password;
-		this.roles=roles;
+		this.roles = roles;
 	}
-	
-    public OperatoerDTO(OperatoerDTO opr)
-    {
-    	this.oprId = opr.getOprId();
-    	this.oprNavn = opr.getOprNavn();
-    	this.ini = opr.getIni();
-    	this.cpr = opr.getCpr();
-    	this.password = opr.getPassword();
-    	this.roles=opr.getRoles();
-    }
-    
-    public int getOprId() { return oprId; }
-	public void setOprId(int oprId) { this.oprId = oprId; }
-	public String getOprNavn() { return oprNavn; }
-	public void setOprNavn(String oprNavn) { this.oprNavn = oprNavn; }
-	public String getIni() { return ini; }
-	public void setIni(String ini) { this.ini = ini; }
-	public String getCpr() { return cpr; }
-	public void setCpr(String cpr) { this.cpr = cpr; }
-	public String getPassword() { return password; }
-	public void addRole(String role){this.roles.add(role);}
-	public void setPassword(String password) { this.password = password; }
-	public List<String> getRoles() {return roles;}
-	public void setRoles(List<String> roles) {this.roles = roles;}
-	public String toString() { return "Operatoer ID:	" + oprId +"\n"+"Username:	" + oprNavn + "\n"+ "Initials:	" + ini + "\n"+"Role(s):\t" + roles + "\n"+"CPR:		"+cpr+"\n"+"Password:	"+password + "\n \n"; }
+
+	public OperatoerDTO(OperatoerDTO opr) {
+		this.oprId = opr.getOprId();
+		this.oprNavn = opr.getOprNavn();
+		this.ini = opr.getIni();
+		this.cpr = opr.getCpr();
+		this.password = opr.getPassword();
+		this.roles = opr.getRoles();
+	}
+
+	public int getOprId() {
+		return oprId;
+	}
+
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
+	}
+
+	public String getOprNavn() {
+		return oprNavn;
+	}
+
+	public void setOprNavn(String oprNavn) {
+		this.oprNavn = oprNavn;
+	}
+
+	public String getIni() {
+		return ini;
+	}
+
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+
+	public String getCpr() {
+		return cpr;
+	}
+
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void addRole(RoleDTO role) {
+		this.roles.add(role);
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<RoleDTO> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<RoleDTO> roles) {
+		this.roles = roles;
+	}
+
+	public String toString() {
+		return "Operatoer ID:	" + oprId + "\n" + "Username:	" + oprNavn + "\n" + "Initials:	" + ini + "\n"
+				+ "Role(s):\t" + roles + "\n" + "CPR:		" + cpr + "\n" + "Password:	" + password + "\n \n";
+	}
 }
