7f1fbee681ff10a438afb8bac4e1ceb521c7bfe6
emilyrasmussen <emilyrasmussen@emilys-air.clients.wireless.dtu.dk>
2017-04-05 14:27:18 +0200

    j

--- 15_CDIO_D3/WebContent/JS.js
+++ 15_CDIO_D3/WebContent/JS.js
@@ -1,24 +1,32 @@
-
 function validateForm() {
         var un = document.loginform.usr.value;
         var pw = document.loginform.pword.value;
         var username = "username"; 
         var password = "password";
         if ((un == username) && (pw == password)) {
-            return true;
+        	document.getElementById('login').style.display = "none";
+    		document.getElementById('admin').style.display = "";
         }
         else {
             alert ("Login was unsuccessful, please check your username and password");
             return false;
         }
   }
- 
 
-function goTo () {
-	if (validateForm == true) 
-		document.getElementById('login').style.display = "none";
-		document.getElementById('admin').style.display = "block";
+function handleSubmit() {
+	getElementsByClassName()
 	
-
+	
 }
 
+
+function objectifyForm(formArray) {//serialize data function
+
+	  var returnArray = {};
+	  for (var i = 0; i < formArray.length; i++){
+	    returnArray[formArray[i]['name']] = formArray[i]['value'];
+	  }
+	  return returnArray;
+	}
+
+var formData = JSON.stringify($("#loginform").serializeArray());
